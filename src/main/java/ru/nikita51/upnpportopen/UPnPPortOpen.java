82ca6ba16a8f41f2869dfc55a100ce98d07aab8a
nikita51bot <nikita.kjiaccuk@mail.ru>
2024-04-22 19:47:42 +0300

    Update version and fix some bugs

--- src/main/java/ru/nikita51/upnpportopen/UPnPPortOpen.java
+++ src/main/java/ru/nikita51/upnpportopen/UPnPPortOpen.java
@@ -1,5 +1,6 @@
 package ru.nikita51.upnpportopen;
 
+import com.dosse.upnp.PortMappingEntity;
 import com.dosse.upnp.UPnP;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,32 +9,48 @@ import org.bukkit.scheduler.BukkitTask;
 public final class UPnPPortOpen extends JavaPlugin {
 
     private int port = 25565;
-    private static final int LEAST_DURATION = 3720;
-    private static final int TASK_PERIOD = 3600;
+    private static final int LEAST_DURATION = 3600;
+    private static final int TASK_PERIOD = 3500;
 
     @Override
     public void onEnable() {
         int taskPeriodTicks = TASK_PERIOD * 20;
         port = getServer().getPort();
+        String description = "MinecraftServer TCP (" + port + ")";
         Bukkit.getScheduler().runTaskLaterAsynchronously(this, () -> {
-            String message;
             String externalIp = UPnP.getExternalIP() + (port == 25565 ? "" : ":" + port);
-            if (UPnP.isMappedTCP(port) || UPnP.openPortTCP(port, "MinecraftServer(" + port + ")", LEAST_DURATION)) {
-                message = "Порт был успешно открыт, скинь ip друзьям: " + externalIp;
-            } else {
-                message = "К сожалению не удалось открыть порт, попробуйте открыть его вручную, ваш ip:" + externalIp;
+            PortMappingEntity entity = UPnP.getPortMappingTCP(port);
+            if (entity != null && entity.getLeaseDuration() == 0) {
+                prettyPrint("Порт был успешно открыт, скинь ip друзьям: " + externalIp);
+                return;
             }
-            StringBuilder selectMessage = new StringBuilder();
-            for (int i = 0; i <= message.length(); i++) {
-                selectMessage.append('#');
+            UPnP.closePortTCP(port);
+            if (UPnP.openPortTCP(port, description, LEAST_DURATION)) {
+                prettyPrint("Порт был успешно открыт, скинь ip друзьям: " + externalIp);
+                Bukkit.getScheduler().runTaskTimerAsynchronously(this,
+                        () -> {
+                            PortMappingEntity e = UPnP.getPortMappingTCP(port);
+                            if (e == null || e.getLeaseDuration() != 0) {
+                                UPnP.closePortTCP(port);
+                                UPnP.openPortTCP(port, description, LEAST_DURATION);
+                            }
+                        },
+                        taskPeriodTicks,
+                        taskPeriodTicks
+                );
+                return;
             }
-            getLogger().info("\n" + selectMessage + "\n\n" + message + "\n\n" + selectMessage);
-            Bukkit.getScheduler().runTaskTimerAsynchronously(this,
-                    () -> UPnP.openPortTCP(port, "MinecraftServer(" + port + ")", LEAST_DURATION),
-                    taskPeriodTicks,
-                    taskPeriodTicks
-            );
+            prettyPrint("К сожалению не удалось открыть порт, попробуйте открыть его вручную, ваш ip:" + externalIp);
         }, 1);
+    }
+
+    public void prettyPrint(String message) {
+        StringBuilder selectMessage = new StringBuilder();
+        for (int i = 0; i <= message.length(); i++) {
+            selectMessage.append('#');
+        }
+        message = String.format("%n%s%n%n%s%n%n%s", selectMessage, message, selectMessage);
+        getLogger().info(message);
     }
 
     @Override
